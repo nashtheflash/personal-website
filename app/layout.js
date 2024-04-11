f175333209dfb382252a919803ad66fe8a24050a
Nash <nashb1323@gmail.com>
2024-04-11 16:30:16 -0500

    home page work

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
+import { inter, mori } from "../lib/fonts";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${inter.variable} ${mori.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
