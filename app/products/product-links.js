305b88abf25f1cdfcb241d43939ee382da94a68e
Nash <nashb1323@gmail.com>
2024-06-05 14:38:21 -0500

    i started actionpacker artical

---
+++ app/products/product-links.js
@@ -0,0 +1,13 @@
+//TOOLS
+export const DrillBits = ({title='DrillBits'} = {}) => <a href='https://amzn.to/3yMPtlP' target="_blank">{title}</a>
+export const Drill = ({title='Drill'} = {}) => <a href='https://amzn.to/4bKPfuh' target="_blank">{title}</a>
+export const BoltCutters = ({title='Bolt Cutters'} = {}) => <a href='https://amzn.to/3RhwVAK' target="_blank">{title}</a>
+export const AngleGrinder = ({title='Angle Ginder'} = {}) => <a href='https://amzn.to/3KvSMR1' target="_blank">{title}</a>
+
+//ACTIONPACKER
+export const ActionPacker = ({title='ActionPaker'} = {}) => <a href='https://amzn.to/3yWOTSt' target="_blank">{title}</a>
+
+//MAG-LIGHT
+export const lightBulb ='';
+export const reflectiveFunnel ='';
+export const glass ='';
