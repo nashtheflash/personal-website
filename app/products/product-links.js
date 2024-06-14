c0fa77c8f377ae9c08c63badb1d5cd9365644368
Nash <nashb1323@gmail.com>
2024-06-13 20:07:44 -0500

    added powerbox artical

--- app/products/product-links.js
+++ app/products/product-links.js
@@ -1,5 +1,8 @@
+//Consumables
+export const PlumbersPutty = ({title='Plumbers Putty'} = {}) => <a href='https://amzn.to/3VvHUIa' target="_blank">{title}</a>
+
 //TOOLS
-export const DrillBits = ({title='DrillBits'} = {}) => <a href='https://amzn.to/3yMPtlP' target="_blank">{title}</a>
+export const DrillBits = ({title='Drill Bits'} = {}) => <a href='https://amzn.to/3yMPtlP' target="_blank">{title}</a>
 export const Drill = ({title='Drill'} = {}) => <a href='https://amzn.to/4bKPfuh' target="_blank">{title}</a>
 export const BoltCutters = ({title='Bolt Cutters'} = {}) => <a href='https://amzn.to/3RhwVAK' target="_blank">{title}</a>
 export const AngleGrinder = ({title='Angle Ginder'} = {}) => <a href='https://amzn.to/3KvSMR1' target="_blank">{title}</a>
@@ -15,3 +18,6 @@ export const Handles = ({title='Handles'} = {}) => <a href='https://amzn.to/3xdj
 
 //MAG-LIGHT
 export const ButtonRubber = ({title='Button Rubber'} = {}) => <a href='https://amzn.to/3wWol3r' target="_blank">{title}</a>
+
+//Ryobi Link
+export const RyobiLinkBoxMedium = ({title='Ryobi Link Medium Box'} = {}) => <a href='https://amzn.to/3VqOJKO' target="_blank">{title}</a>
