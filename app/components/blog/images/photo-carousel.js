f683dba5dde4c1947c03dc03209a1aad223688a9
Nash <nashb1323@gmail.com>
2024-09-12 10:06:43 -0500

    fixed photo bugs

--- app/components/blog/images/photo-carousel.js
+++ app/components/blog/images/photo-carousel.js
@@ -1,53 +1,42 @@
 'use client'
 import Image from 'next/image'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function PhotoCarousel({photoUrls}) {
     const totalPictures = photoUrls.length;
-
-    const [prevPic, setPrevPic] = useState(totalPictures - 1);
-    const [nextPic, setNextPic] = useState(1);
+    const [currentPic, setCurrentPic] = useState(0);
 
     const goTo = (event, type, totalPics) => {
         event.preventDefault()
         const btn = event.currentTarget;
 
         const carousel = document.querySelector('.carousel')
-
         const href = btn.getAttribute('href')
         const target = carousel.querySelector(href)
         const left = target.offsetLeft
         carousel.scrollTo({ left: left })
 
-        //Lands on final pic   
-        if (nextPic == totalPictures - 1 && type == 'next') {
-            setNextPic(0);
-            setPrevPic(totalPictures - 2);
-        } 
+        if (type == 'next') {
+            if (currentPic == totalPictures - 1) {
+                setCurrentPic(0);
+            } else {
+                setCurrentPic((prev) => prev + 1);
+            }
+        }
         
-        if (nextPic != totalPictures - 1 && type == 'next') {
-            setNextPic((prev) => {
-                 setPrevPic(prev <= 0 ? totalPictures - 1 : prev - 1);
-                 return prev + 1;
-            })
-        } 
-
-        if (prevPic == 0 && type == 'previous') {
-            setPrevPic((prev) => {
-                setNextPic(prev + 1);
-                return totalPictures - 1
-            })
+        if (type == 'previous') {
+            if (currentPic == 0) {
+                setCurrentPic(totalPictures - 1);
+            } else {
+                setCurrentPic((prev) => prev - 1);
+            }
         }
 
-        if (prevPic != 0 && type == 'previous') {
-            setNextPic((prev) => prev - 1);
-            setPrevPic((prev) => prev - 1);
-        }
     }
 
     return(
         <div className='flex justify-center items-center px-10'>
-            <a href={`#slide${prevPic}`} onClick={(e) => goTo(e, 'previous', totalPictures)} className="btn btn-circle">❮</a>
+            <a href={`#slide${getPreviousPic(currentPic, totalPictures)}`} onClick={(e) => goTo(e, 'previous', totalPictures)} className="btn btn-circle">❮</a>
             <div className="carousel w-full mt-2">
                 { photoUrls && photoUrls.map((photo, index) => (
                     <div id={`slide${index}`} key={index} className="carousel-item justify-center items-center relative w-full">
@@ -63,7 +52,7 @@ export function PhotoCarousel({photoUrls}) {
                 ))
                 }
             </div>
-            <a href={`#slide${nextPic}`} onClick={(e) => goTo(e, 'next', totalPictures)} className="btn btn-circle">❯</a>
+            <a href={`#slide${getNextPic(currentPic, totalPictures)}`} onClick={(e) => goTo(e, 'next', totalPictures)} className="btn btn-circle">❯</a>
         </div>
     )
 }
