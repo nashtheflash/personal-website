2e427dbd21f293cc7391352afa6d17b0abacc395
Nash <nashb1323@gmail.com>
2024-10-04 12:55:00 -0500

    rozarks work 2

--- app/projects/projects/kansas-city-mtn-biking/data/rozarks-data.js
+++ app/projects/projects/kansas-city-mtn-biking/data/rozarks-data.js
@@ -3,12 +3,12 @@ export const rozarksData = {
         rank: 4,
         featuredImage: '/mtn-bike-kc/rozarks/arch.JPEG',
         stats: {
-            distanceMiles: 7.5,
-            numberOfTrails: 5,
+            distanceMiles: 5.6,
+            numberOfTrails: 12,
             flowTrails: 0,
-            downHillTrails: 1,
-            XCTrails: 4,
-            areaDificulty: 1,
+            downHillTrails: 0,
+            XCTrails: 12,
+            areaDificulty: 2,
             traffic: 'low',
             currentStatus: 'Open'
         },
@@ -48,47 +48,47 @@ export const rozarksData = {
                 trailheadName: 'Rosedale Arch Trailhead',
                 gMapLinkUrl: 'https://maps.app.goo.gl/GyTpJ78tinhicBvE9',
                 gMapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d439.47249172690664!2d-94.61505387415842!3d39.063979350688335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c0f1d80e717873%3A0x1f8ae9e9a49e6c06!2sRosedale%20Memorial%20Arch!5e1!3m2!1sen!2sus!4v1727966853076!5m2!1sen!2sus',
-                trailheadDiscription: 'This is the main access point for the trails. From here you can jump right onto the Columbine Connector trail that will take you out to Billy Goat and the Columbine Loop.'
+                trailheadDiscription: "This is the main access point for the western trail system. From here you can jump right onto Martt's Woods!"
             },
             {
                 trailheadName: 'Fisher Park Trailhead',
                 gMapLinkUrl: 'https://maps.app.goo.gl/eY7bDYcxk5Gjgkim6',
                 gMapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d925.3113102201669!2d-94.61634539868233!3d39.05758194807152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c0ee25da252423%3A0x10919d2b0265509b!2sFisher%20Park!5e1!3m2!1sen!2sus!4v1727966678744!5m2!1sen!2sus',
-                trailheadDiscription: 'I would call this secondary access. From here you will have to head down the paved path across the bridge and twords the woods. You will see the entrance to Scape Goat. Ride up Scape Goat to access the rest of the trail system.'
+                trailheadDiscription: "This is another access point for the western trail system."
             },
             {
                 trailheadName: 'Community Center Trailhead',
                 gMapLinkUrl: 'https://maps.app.goo.gl/WZwrpfaEuFJhabxf7',
                 gMapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d839.5016291615973!2d-94.60045057511721!3d39.060290703362405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c0efd45b620fa1%3A0xc0b96f9d7f559641!2sWestport%20Roanoke%20KC%20Parks%20Community%20Center!5e1!3m2!1sen!2sus!4v1727966960439!5m2!1sen!2sus',
-                trailheadDiscription: 'I would call this secondary access. From here you will have to head down the paved path across the bridge and twords the woods. You will see the entrance to Scape Goat. Ride up Scape Goat to access the rest of the trail system.'
+                trailheadDiscription: "This is the main access point for the eastern trail system. From here you can head over to the bluff and jump on the trails!!"
             }
         ],
         faqHighlights: [
             {
-                title: 'Columbine Bypass',
-                text: 'is a nice jump trail located in the middle of the trail system. Great place for laps!'
+                title: "Marty's Woods",
+                text: 'is the longest, cleanest, and probably my favrite trail in the system.' 
             },
             {
-                title: 'Columbine Loop',
-                text: 'is best riden counter clockwise'
+                title: 'The Rosedale Memorial Arch',
+                text: 'is worth going up to. It offers a great view of the Kansas City Skyline.'
             },
             {
-                title: 'Technical Loop',
-                text: 'is as chossy as this trail system gets. I prefer riding this trail counter clockwise.'
+                title: 'Roanoke Park Cave',
+                text: 'you never know whats going to crawel out of there.'
             }
         ],
         faqFoodDrink: [
             {
                 title: 'Best post ride beer?',
-                text: 'Red Crow Brewing is the hands down winner. I would go with an Isabelle & a Spicy Honey Mustard Chicken sandwich'
+                text: "is Tanner's. I would recommend biking over so you dont have to park."
             },
             {
-                title: 'Lunch Time?',
-                text: "Bonito Michoacán is a grocery story that has a small cafe that serves authentic mexican food!"
+                title: '39th Street',
+                text: "has about every type of food that you can image on it. Its good to scout out options wile making the 39th Street Traverse."
             },
             {
-                title: "BB's Grill",
-                text: "BB's is better than I would have thought. Good beer selection and the Jalapeno Popper Chicken Sandwich is to die for."
+                title: "Genghis Khan Mongolian Grill",
+                text: "offers grat food. I have to mention that my sister ate on of the crackers infron of the Buddha statue when she was 8.They got pissed!"
             }
         ],
     }
