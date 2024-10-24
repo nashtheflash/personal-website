dd2df7b851c17d7b582a64941ce069c50bf8f65a
Nash <nashb1323@gmail.com>
2024-10-23 23:24:35 -0500

    mopac work

---
+++ app/projects/projects/kansas-city-mtn-biking/products.js
@@ -0,0 +1,4 @@
+export const Dynaplug = ({title='Dynaplug'} = {}) => <a href='https://amzn.to/3BUtv1W' target="_blank">{title}</a>
+export const BikePump = ({title='Bike Pump'} = {}) => <a href='https://amzn.to/3YnHUeK' target="_blank">{title}</a>
+export const MultiTool = ({title='Multi Tool'} = {}) => <a href='https://amzn.to/4e0VGcK' target="_blank">{title}</a>
+export const BearSpray = ({title='Bear Spray'} = {}) => <a href='https://amzn.to/3BVI2ua' target="_blank">{title}</a>
