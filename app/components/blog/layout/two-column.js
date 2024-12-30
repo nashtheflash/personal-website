ac84e1f5ebc50a65b4dd51d9817d637f4d48a519
Nash <nashb1323@gmail.com>
2024-12-30 14:42:40 -0600

    blog work

--- app/components/blog/layout/two-column.js
+++ app/components/blog/layout/two-column.js
@@ -1,6 +1,6 @@
 export function TwoColumn({children}){
     return(
-        <div className="flex flex-col sm:flex-row justify-between items-center sm:items-start w-full">
+        <div className="flex flex-col gap-2 sm:flex-row justify-between items-center sm:items-start w-full">
             {children}
         </div>
     )
