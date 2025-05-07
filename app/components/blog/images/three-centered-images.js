360122a7778419f0688c4ea0055115fcc2bb6087
Nash <nashb1323@gmail.com>
2025-05-07 14:18:54 -0500

    rock island katy work

--- app/components/blog/images/three-centered-images.js
+++ app/components/blog/images/three-centered-images.js
@@ -12,9 +12,9 @@ export function ThreeCenteredImages({image, altText, titles, subtitles, width, h
     const altTextThree = altText[2];
     
     return (
-        <div className='flex justify-center items-center gap-2 w-full h-full'>
-            <div className={`relative group ${height} ${width}`}>
-                <MdxImage src={imageOne} alt={altTextOne} />
+        <div className='flex justify-center items-center gap-2 w-full h-fit'>
+            <div className={`relative group h-fit`}>
+                <MdxImage src={imageOne} alt={altTextOne} height={height} width={width}/>
                     <div className='absolute left-1 bottom-1 w-fit h-fit px-4 py-0 text-white bg-[#bdd2c9] opacity-0 group-hover:opacity-100 transition-opacity ease-in-out delay-150 duration-300 [clip-path:polygon(0_0,100%_0%,95%_100%,0_100%)]'>
                         {
                             titles && titles[0] ? <Title title={titles[0]} subtitle={subtitles && subtitles[0] ? subtitles[0] : ''}/> : <></>
@@ -22,7 +22,7 @@ export function ThreeCenteredImages({image, altText, titles, subtitles, width, h
                     </div>
             </div>
             <div className={`relative group ${height} ${width}`}>
-                <MdxImage src={imageTwo} alt={altTextTwo} />
+                <MdxImage src={imageTwo} alt={altTextTwo} height={height} width={width}/>
                     <div className='absolute left-1 bottom-1 w-fit h-fit px-4 py-0 text-white bg-[#bdd2c9] opacity-0 group-hover:opacity-100 transition-opacity ease-in-out delay-150 duration-300 [clip-path:polygon(0_0,100%_0%,95%_100%,0_100%)]'>
                         {
                             titles && titles[1] ? <Title title={titles[1]} subtitle={subtitles && subtitles[1] ? subtitles[1] : ''}/> : <></>
@@ -30,7 +30,7 @@ export function ThreeCenteredImages({image, altText, titles, subtitles, width, h
                     </div>
             </div>
             <div className={`relative group ${height} ${width}`}>
-                <MdxImage src={imageThree} alt={altTextThree} />
+                <MdxImage src={imageThree} alt={altTextThree} height={height} width={width}/>
                     <div className='absolute left-1 bottom-1 w-fit h-fit px-4 py-0 text-white bg-[#bdd2c9] opacity-0 group-hover:opacity-100 transition-opacity ease-in-out delay-150 duration-300 [clip-path:polygon(0_0,100%_0%,95%_100%,0_100%)]'>
                         {
                             titles && titles[2] ? <Title title={titles[2]} subtitle={subtitles && subtitles[2] ? subtitles[2] : ''}/> : <></>
