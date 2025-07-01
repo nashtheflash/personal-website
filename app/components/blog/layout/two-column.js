dbe7eb95918fe05f5705754e66a0564b11334a16
Nash <nashb1323@gmail.com>
2025-07-01 12:55:18 -0500

    added no shift strava and garmin

--- app/components/blog/layout/two-column.js
+++ app/components/blog/layout/two-column.js
@@ -1,6 +1,7 @@
-export function TwoColumn({children}){
+export function TwoColumn({children, className}){
+
     return(
-        <div className="flex flex-col gap-2 sm:flex-row justify-between items-center sm:items-start w-full">
+        <div className={`flex flex-col gap-2 sm:flex-row justify-between items-center sm:items-start w-full ${className}`}>
             {children}
         </div>
     )
