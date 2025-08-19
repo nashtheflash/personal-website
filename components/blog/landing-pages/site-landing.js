fcb8457d98f953180ebd84d9db45334c6b2b25bd
Nash <nashb1323@gmail.com>
2025-08-19 18:21:19 -0500

    fixed layout shift on /

--- components/blog/landing-pages/site-landing.js
+++ components/blog/landing-pages/site-landing.js
@@ -6,13 +6,15 @@ export function SiteLanding() {
     return(
             <div className="w-full h-[calc(100vh-64px)] overflow-hidden">
                 <div className="flex flex-col justify-center items-center h-full w-full">
-                    <Image
-                        src={hippyHokusaiNashBrownsLogo}
-                        alt='NashBrowns Logo'
-                        width={500}
-                        height={500}
-                        className='animate-spin-smooth'
-                    />
+                    <div className="h-500 w-500 overflow-hidden">
+                        <Image
+                            src={hippyHokusaiNashBrownsLogo}
+                            alt='NashBrowns Logo'
+                            width={500}
+                            height={500}
+                            className='animate-spin-smooth'
+                        />
+                    </div>
                 </div>
             </div>
     )
