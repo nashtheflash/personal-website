8627506bbbc1c415094321fcefcbc4881f15669c
Nash Bostwick <nashb1323@gmail.com>
2026-05-08 22:27:07 -0600

    working on surviving winter. claude fixed some issues with the image component. added script for claude to import images from a storage folder

--- components/blog/images/centered-image.js
+++ components/blog/images/centered-image.js
@@ -1,13 +1,15 @@
 import { MdxImage } from '@/components/blog'
 
 export function CenteredImage({image, altText, title, subtitle, width, height}) {
-   
+
     return (
         <>
             <div className='flex justify-center items-start w-full'>
-                <div className={`relative group`}>
-                    <MdxImage src={image} alt={altText} width={width} height={height} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 800px"/>
-                    <div className='absolute left-1 bottom-1 w-fit h-fit px-4 py-0 text-white bg-[#bdd2c9] opacity-0 group-hover:opacity-100 transition-opacity ease-in-out delay-150 duration-300 [clip-path:polygon(0_0,100%_0%,95%_100%,0_100%)]'>
+                <div className='grid group w-fit not-prose'>
+                    <div className='col-start-1 row-start-1'>
+                        <MdxImage src={image} alt={altText} width={width} height={height} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 800px"/>
+                    </div>
+                    <div className='col-start-1 row-start-1 self-end justify-self-start z-10 w-fit h-fit px-4 py-0 text-white bg-[#bdd2c9] opacity-0 group-hover:opacity-100 transition-opacity ease-in-out delay-150 duration-300 [clip-path:polygon(0_0,100%_0%,95%_100%,0_100%)]'>
                         {
                             title ? <Title title={title} subtitle={subtitle}/> : <></>
                         }
@@ -20,7 +22,7 @@ export function CenteredImage({image, altText, title, subtitle, width, height}) 
 
 
 function Title({title, subtitle}) {
-    
+
     return (
         <div className='flex flex-col justify-start items-start'>
             <span className='text-4xl font-bold pr-3'>{title}</span>
@@ -32,7 +34,7 @@ function Title({title, subtitle}) {
 }
 
 function SubTitle({subtitle}) {
-    
+
     return (
         <span className='text-md font-semibold'>{subtitle}</span>
     )
